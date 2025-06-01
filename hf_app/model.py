7ddbc19184c8adc756328a585154174e2d05fb50
rhkraptor <hussnainkhalil2001@gmail.com>
2025-06-01 21:04:23 +0200

    bug fix

--- hf_app/model.py
+++ hf_app/model.py
@@ -1,25 +1,14 @@
-import torch
 import torch.nn as nn
 import torchvision.models as models
 
 class BananaOrAppleClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.base_model = models.resnet18(pretrained=True)
-
-        # Freeze all first
+        self.base_model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
         for param in self.base_model.parameters():
             param.requires_grad = False
-
-        # Unfreeze last residual block
-        for param in self.base_model.layer4.parameters():
-            param.requires_grad = True
-
         self.base_model.fc = nn.Sequential(
-            nn.Linear(self.base_model.fc.in_features, 256),
-            nn.ReLU(),
-            nn.Dropout(0.4),
-            nn.Linear(256, 128),
+            nn.Linear(self.base_model.fc.in_features, 128),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(128, 3)
