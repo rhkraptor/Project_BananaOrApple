9a4c79d63028a5b29cd96d6fa0e5b7c5ddff9448
rhkraptor <hussnainkhalil2001@gmail.com>
2025-06-01 09:49:26 +0200

    Fix model import path for Hugging Face

--- hf_app/app.py
+++ hf_app/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'training', 'src')))
+
 import gradio as gr
 import torch
 import torch.nn.functional as F
