323a22a351cf70d488838ba1216938601c38d6b1
rhkraptor <hussnainkhalil2001@gmail.com>
2025-06-01 13:40:09 +0200

    Add model.py to hf_app to fix Hugging Face import error

---
+++ hf_app/model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class BananaOrAppleClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.base_model = models.resnet18(pretrained=True)
+        for param in self.base_model.parameters():
+            param.requires_grad = False
+        self.base_model.fc = nn.Sequential(
+            nn.Linear(self.base_model.fc.in_features, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 3)  # 3 output classes
+        )
+
+    def forward(self, x):
+        return self.base_model(x)
